c2d64e38dc255bd3bdfb2687d15a5e5a0a4c3806
Katrina <74234305+Katrina-Panina@users.noreply.github.com>
2021-08-04 18:55:19 +0300

    fix

---
+++ src/js/Vampire.js
@@ -0,0 +1,13 @@
+import Character from "./Character";
+
+export default class Vampire extends Character {
+  constructor(level) {
+    super(level, "vampire");
+    this.attack = 25;
+    this.defence = 25;
+    this.isPlayer = false;
+    this.step = 2;
+    this.range = 2;
+  }
+}
+ 
