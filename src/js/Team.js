3e1bb510c73d8b70db62d5c0a2d415a15f82acb6
Katrina <74234305+Katrina-Panina@users.noreply.github.com>
2021-08-04 19:17:53 +0300

    fix

--- src/js/Team.js
+++ src/js/Team.js
@@ -1,3 +1,13 @@
-export default class Team {
+import Bowman from "./Bowman";
+import Swordsman from "./Swordsman";
+import Magician from "./Magician";
+import Daemon from "./Daemon";
+import Vampire from "./Vampire";
+import Undead from "./Undead";
 
+export default class Team {
+  constructor() {
+    this.playerTeams = [Bowman, Swordsman, Magician];
+    this.npcTeams = [Daemon, Vampire, Undead];
+  }
 }
