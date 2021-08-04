4ec9a0191d0a9982d1cf1ab41700e1397a02b193
Katrina <74234305+Katrina-Panina@users.noreply.github.com>
2021-08-04 18:53:05 +0300

    fix

--- src/js/generators.js
+++ src/js/generators.js
@@ -1,3 +1,5 @@
+import PositionedCharacter from "./PositionedCharacter";
+
 /**
  * Generates random characters
  *
@@ -6,9 +8,50 @@
  * @returns Character type children (ex. Magician, Bowman, etc)
  */
 export function* characterGenerator(allowedTypes, maxLevel) {
-  // TODO: write logic here
+  while (true) {
+    const randomTeam = Math.floor(Math.random() * allowedTypes.length);
+    const randomLevel = 1 + Math.floor(Math.random() * maxLevel);
+    yield new allowedTypes[randomTeam](randomLevel);
+  }
 }
 
-export function generateTeam(allowedTypes, maxLevel, characterCount) {
-  // TODO: write logic here
+export function generateCoordinates(type, boardSize = 8) {
+  return new Array(boardSize).fill(0).reduce((acc, prev, index) => {
+    if (type === "player") {
+      acc.push(index * boardSize, index * boardSize + 1);
+    } else {
+      acc.push(
+        index * boardSize + boardSize - 2,
+        index * boardSize + boardSize - 1
+      );
+    }
+    return acc;
+  }, []);
+}
+
+export function generateTeam(
+  allowedTypes,
+  maxLevel,
+  characterCount,
+  boardSize
+) {
+  const playerCoordinates = generateCoordinates("player", boardSize);
+  const npcCoordinates = generateCoordinates("npc", boardSize);
+  let position;
+  let idx;
+  const teams = [];
+  for (let key = 0; key < characterCount; key += 1) {
+    const { value } = characterGenerator(allowedTypes, maxLevel).next();
+    if (value.isPlayer) {
+      idx = Math.floor(Math.random() * playerCoordinates.length);
+      position = playerCoordinates[idx];
+      playerCoordinates.splice(idx, 1);
+    } else {
+      idx = Math.floor(Math.random() * npcCoordinates.length);
+      position = npcCoordinates[idx];
+      npcCoordinates.splice(idx, 1);
+    }
+    teams.push(new PositionedCharacter(value, position));
+  }
+  return teams;
 }
