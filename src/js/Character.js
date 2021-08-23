fc3c0ac8ce7fe2b27a476897b4307ea3f150f5ae
Katrina <74234305+Katrina-Panina@users.noreply.github.com>
2021-08-23 17:50:36 +0300

    fix

--- src/js/Character.js
+++ src/js/Character.js
@@ -1,18 +1,18 @@
 export default class Character {
-  constructor(level, type = "generic") {
+  constructor(level, type = 'generic') {
     this.level = level;
     this.attack = 0;
     this.defence = 0;
     this.health = 100;
     this.type = type;
-    if (new.target.name === "Character") {
-      throw new Error("You cannot create instances from the Character class");
+    if (new.target.name === 'Character') {
+      throw new Error('You cannot create instances from the Character class');
     }
   }
 
   levelUp() {
     if (!this.health) {
-      throw new Error("Нельзя повысить уровень умершего");
+      throw new Error('Нельзя повысить уровень умершего');
     }
     this.level += 1;
     this.statsUp();
