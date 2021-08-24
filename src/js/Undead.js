b31b2d8eef3c0a36d66b159a65d9d2933e0896ef
Katrina <74234305+Katrina-Panina@users.noreply.github.com>
2021-08-24 18:41:48 +0300

    fix

--- src/js/Undead.js
+++ src/js/Undead.js
@@ -1,8 +1,8 @@
-import Character from "./Character";
+import Character from './Character';
 
 export default class Undead extends Character {
   constructor(level) {
-    super(level, "undead");
+    super(level, 'undead');
     this.attack = 40;
     this.defence = 10;
     this.isPlayer = false;
